3e25f5c0210b7cb2221e19c2a4e0c73f91841f0f
LinenLynx51926 <101571879+LinenLynx51926@users.noreply.github.com>
2025-02-09 16:16:40 -0300

    player.py

--- minepy/minecraft-game/src/player.py
+++ minepy/minecraft-game/src/player.py
@@ -1,36 +1,20 @@
-class Player:
-    def __init__(self, position):
-        self.position = position
-        self.speed = 1.0
-        self.is_jumping = False
-        self.jump_height = 5.0
-        self.camera_angle = 0.0
-
-    def move(self, direction):
-        if direction == "forward":
-            self.position[0] += self.speed
-        elif direction == "backward":
-            self.position[0] -= self.speed
-        elif direction == "left":
-            self.position[1] -= self.speed
-        elif direction == "right":
-            self.position[1] += self.speed
-
-    def jump(self):
-        if not self.is_jumping:
-            self.is_jumping = True
-            # Logic for jumping can be added here
-
-    def run(self):
-        self.speed = 2.0
-
-    def walk(self):
-        self.speed = 1.0
-
-    def rotate_camera(self, angle):
-        self.camera_angle += angle
-        # Logic for camera rotation can be added here
-
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from blocks import Block
+class Player(FirstPersonController):
+    def __init__(self):
+        super().__init__()
+        self.cursor = Entity(parent=camera.ui, model='quad', color=color.pink, scale=.008, rotation_z=45)
+        self.current_block = 'grass'
+        
     def update(self):
-        # Update player state, handle jumping logic, etc.
-        pass
+        super().update()
+        
+        if held_keys['left mouse'] or held_keys['right mouse']:
+            hit_info = raycast(camera.world_position, camera.forward, distance=10)
+            if hit_info.hit:
+                if held_keys['left mouse']:
+                    destroy(hit_info.entity)
+                elif held_keys['right mouse']:
+                    new_position = hit_info.entity.position + hit_info.normal
+                    Block(position=new_position, block_type=self.current_block)
